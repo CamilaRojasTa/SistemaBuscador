12d11c768f0a77d363e62d04b4c147c9e06725bb
CAMILA PATRICIA ROJAS TAPIA <CAMILA.ROJASTA@correoaiep.cl>
2021-09-15 13:00:50 -0300

    Cambios20210915 avances

---
+++ SistemaBuscador/Controllers/UsuariosController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaBuscador.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaBuscador.Controllers
+{
+    public class UsuariosController: Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult NuevoUsuario()
+        {
+            return View();
+
+        }
+        [HttpPost]
+        public IActionResult NuevoUsuario(UsuarioCreacionModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ SistemaBuscador/Models/UsuarioCreacionModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaBuscador.Models
+{
+    public class UsuarioCreacionModel
+    {
+        [Display(Name="Nombre de Usuario")]
+        public string NombreUsuario { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        [Display(Name = "Rol")]
+        public int RolId { get; set; }
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+        [Display(Name = "Repetir Contraseña")]
+        public string RePassword { get; set; }
+    }
+}
